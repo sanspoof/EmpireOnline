c506a058700e5836e7b4021836d2996aa3680bfe
sanspoof <alex.spencer@live.com>
2025-03-13 15:35:51 +0000

    Refactor HeroHeader component to display main banner based on Contentful settings

--- src/components/HeroHeader/HeroHeader.jsx
+++ src/components/HeroHeader/HeroHeader.jsx
@@ -16,23 +16,24 @@ function HeroHeader() {
   if (loading['homepageBanner']) return <div>Loading...</div>;
   if (!banners || banners.length === 0) return <div>No data available</div>;
 
-  // array index 1
-  // set a "makeFrontPage" field to true in Contentful to make this banner appear on the front page
-  const banner = banners[1]; 
+  // show as main set in contentful
+  const mainItem = banners.find(item => item.showAsMain === true);
+
+  console.log(mainItem);
 
   return (
     <div className={styles.heroheader}>
       <div className={styles.heroheader__info}>
-        <div className={styles.heroheader__title}>{banner.mainTitle}</div>
+        <div className={styles.heroheader__title}>{mainItem.mainTitle}</div>
         <span className={styles.heroheader__dash}></span>
-        <div className={styles.heroheader__subtitle}>{banner.mainSubtitle}</div>
-        <MyButton text={banner.mainCallToActionButtonText} />
+        <div className={styles.heroheader__subtitle}>{mainItem.mainSubtitle}</div>
+        <MyButton text={mainItem.mainCallToActionButtonText} />
       </div>
       <div className={styles.heroheader__imgwrap}>
-        {banner.mainBannerImage && (
+        {mainItem.mainBannerImage && (
           <img
-            src={banner.mainBannerImage.fields.file.url}
-            alt={banner.mainTitle}
+            src={mainItem.mainBannerImage.fields.file.url}
+            alt={mainItem.mainTitle}
             className={styles.heroheader__img}
           />
         )}
