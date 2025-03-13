9c3733da80e61c8f0893d34e7c305f4d40f58414
sanspoof <alex.spencer@live.com>
2025-03-13 10:21:32 +0000

    Add LoadingSpinner component and update ReviewList to use it for loading state

--- src/components/ReviewList/ReviewList.jsx
+++ src/components/ReviewList/ReviewList.jsx
@@ -2,9 +2,11 @@ import React, { useContext, useEffect } from 'react';
 import { ContentfulContext } from './../../context/ContentfulContext';
 import css from './ReviewsList.module.scss';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import Loading from '../UI/LoadingSpinner/LoadingSpinner';
  
 // ReviewList Component
 function ReviewList() {
+
   const { content, fetchContent, loading } = useContext(ContentfulContext);
 
   useEffect(() => {
@@ -16,14 +18,14 @@ function ReviewList() {
   // Get reviews from the context
   const reviews = content['reviews']; // contenful content type
 
-  if (loading['reviews']) return <div>Loading...</div>;  // Show loading state
+  if (loading['reviews']) return <Loading/>;  // Show loading state
   
   if (!reviews || reviews.length === 0) return <div>No data available</div>;  // Handle empty state
 
   return (
     <div className={css.reviewlist}>
       {reviews.map((review) => (
-        // Pass the fields from Contentful as props to the ReviewItem component
+        // Passes the fields from Contentful as props to the ReviewItem component
         <ReviewItem 
             key={review.id} 
             data={review} 
---
+++ src/components/UI/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,11 @@
+// Loading.js
+import React from 'react';
+import css from './LoadingSpinner.module.scss';
+
+function Loading() {
+  return (
+    <div className={css.loading}></div>
+  );
+}
+
+export default Loading; 
