27ce22ad5dda0ad311de9aea653eac57eee82a7b
Alex Spencer <alex.spencer@live.com>
2025-07-16 13:45:55 +0100

    Update NewsList component to adjust grid column size for improved layout

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -25,7 +25,7 @@ import { ContentfulContext } from './../../context/ContentfulContext';
       <section className="grid col-span-full md:col-span-6 lg:col-span-8 gap-2 content-start px-6 pt-4 md:pl-6 md:pr-0">
       <h3 className="heading-primary text-gray-900 dark:text-white">Latest Movie News</h3>
       <span className="block w-12 border-b border-gray-900 dark:border-white mb-2"></span>
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(260px,1fr))] gap-6">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(270px,1fr))] gap-6">
         {news.map((newsItem) => (
           <NewsItem 
           key={newsItem.id} 
